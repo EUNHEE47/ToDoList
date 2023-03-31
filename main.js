028ca39fde7c12901269289adf8f28d007964d22
PARK EUN HEE <mii0329@naver.com>
2023-03-31 20:56:03 +0900

    input chng

--- main.js
+++ main.js
@@ -8,41 +8,98 @@
 
 let taskInput = document.getElementById("task-input");
 let addBtn = document.getElementById("add-button");
+let tabs = document.querySelectorAll(".task-tabs div");
 let taskList = [];
+let mode = "all";
+let filterList = [];
+let underLine = document.getElementById("under-line");
 
-// addBtn.addEventListener("click", addTask);
+for (i = 1; i < tabs.length; i++) {
+  tabs[i].addEventListener("click", function (event) {
+    filter(event);
+  });
+}
 
-// function addTask() {
+addBtn.addEventListener("click", addTask);
 
-// }
-addBtn.addEventListener("click", function () {
+taskInput.addEventListener("keyup", function (event) {
+  if (window.event.keyCode === 13) {
+    addTask(event);
+  }
+});
+
+// +버튼을 클릭 하면
+function addTask() {
+  if (taskInput.value === "") {
+    alert("내용을 입력해 주세요");
+    return;
+  }
+
   let task = {
     id: randomIDGenerate(),
     taskContent: taskInput.value,
     isComplete: false,
   };
   taskList.push(task);
-  console.log(taskList);
+  taskInput.value = "";
   render();
-});
+}
 
+function filter(event) {
+  mode = event.target.id;
+  filterList = [];
+
+  if (event) {
+    underLine.style.width = event.target.offsetWidth + "px";
+    underLine.style.left = event.target.offsetLeft + "px";
+    underLine.style.top =
+      e.target.offsetTop + (event.target.offsetHeight - 4) + "px";
+  }
+
+  if (mode === "all") {
+    render();
+  } else if (mode === "ongoing") {
+    for (let i = 0; i < taskList.length; i++) {
+      if (taskList[i].isComplete === false) {
+        filterList.push(taskList[i]);
+      }
+    }
+    render();
+  } else if (mode === "done") {
+    for (let i = 0; i < taskList.length; i++) {
+      if (taskList[i].isComplete === true) {
+        filterList.push(taskList[i]);
+      }
+    }
+    render();
+  }
+}
+
+// UI
 function render() {
+  let list = [];
+  if (mode === "all") {
+    list = taskList;
+  } else if (mode === "ongoing" || mode === "done") {
+    list = filterList;
+  }
+
   let resultHTML = "";
-  for (let i = 0; i < taskList.length; i++) {
-    if (taskList[i].isComplete === true) {
+  for (let i = 0; i < list.length; i++) {
+    if (list[i].isComplete === true) {
       resultHTML += `<div class="task">
-                      <div class="task-done">${taskList[i].taskContent}</div>
+                      <div class="task-done">${list[i].taskContent}</div>
                       <div>
-                        <button onclick="toggleComplete('${taskList[i].id}')">Check</button>
-                        <button>Delete</button>
+                        <button onclick="toggleComplete('${list[i].id}')">Check</button>
+                        <button onclick="deleteTask('${list[i].id}')">Delete</button>
                       </div>
                     </div>`;
     } else {
       resultHTML += `<div class="task">
-                      <div>${taskList[i].taskContent}</div>
+                      <div>${list[i].taskContent}</div>
                       <div>
-                        <button onclick="toggleComplete('${taskList[i].id}')">Check</button>
-                        <button>Delete</button>
+                        <button onclick="toggleComplete('${list[i].id}')">Check</button>
+                        <button onclick="deleteTask('${list[i].id}')">Delete</button>
                       </div>
                     </div>`;
     }
@@ -51,11 +108,12 @@ function render() {
   document.getElementById("task-board").innerHTML = resultHTML;
 }
 
+// checkBtn
 function toggleComplete(id) {
   console.log("id:", id);
   for (i = 0; i < taskList.length; i++) {
     if (taskList[i].id === id) {
-      taskList[i].isComplete = true;
+      taskList[i].isComplete = !taskList[i].isComplete;
       break;
     }
   }
@@ -63,6 +121,18 @@ function toggleComplete(id) {
   console.log(taskList);
 }
 
+// deleteBtn
+function deleteTask(id) {
+  for (let i = 0; i < taskList.length; i++) {
+    if (taskList[i].id === id) {
+      taskList.splice(i, 1);
+      break;
+    }
+  }
+  render();
+}
+
+// ID값
 function randomIDGenerate() {
   return "_" + Math.random().toString(36).substr(2, 9);
 }
